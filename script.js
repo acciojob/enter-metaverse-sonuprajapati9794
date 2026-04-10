40fe4aca0ecec45322ad81340f2f5e5b354fa67f
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-10 12:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Get elements
+const statusText = document.getElementById("status");
+const button = document.getElementById("enterBtn");
+
+// Add click event
+button.addEventListener("click", function () {
+  
+  // Change text inside <h1> tag
+  statusText.innerHTML = "<h1>Entered Metaverse</h1>";
+});
